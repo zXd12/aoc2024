a0e53094cea3618b93b92ffd64a300cb1a7c48e9
zXd12 <foucherewen@gmail.com>
2024-12-05 12:49:03 +0100

    opti part1

--- src/question/day4.rs
+++ src/question/day4.rs
@@ -4,76 +4,65 @@ use core::str;
 
 use crate::day_solver::{test_day, DaySolver};
 
-#[derive(Debug)]
-enum State {
-    X,
-    M,
-    A,
-    S,
-}
-
-impl From<State> for u8 {
-    fn from(state: State) -> u8 {
-        match state {
-            State::X => b'X',
-            State::M => b'M',
-            State::A => b'A',
-            State::S => b'S',
-        }
-    }
-}
-
-impl State {
-    fn next(&self) -> State {
-        match self {
-            State::X => State::M,
-            State::M => State::A,
-            State::A => State::S,
-            State::S => unreachable!(),
-        }
-    }
-}
-
 fn part1(input: &str) -> String {
     let mut count = 0;
-    let line_lenght = input.lines().next().unwrap().len() + 1;
+    let line_length = input.lines().next().unwrap().len() + 1;
     let bytes = input.as_bytes();
     for i in 0..bytes.len() {
         if bytes[i] == b'X' {
-            for x in -1..=1 {
-                for y in -1..=1 {
-                    if x == 0 && y == 0 {
-                        continue;
-                    }
-                    if search(bytes, i, line_lenght as i32, (x, y), State::X) {
-                        count += 1;
-                    }
-                }
-            }
+            count += positive_directions(bytes, i, line_length);
+            count += negative_directions(bytes, i, line_length);
         }
     }
     count.to_string()
 }
 
-fn search(bytes: &[u8], i: usize, line_length: i32, direction: (i32, i32), state: State) -> bool {
-    match state {
-        State::S => {
-            return true;
-        }
-        _ => {}
+#[inline]
+fn positive_check(bytes: &[u8], i: usize, step_by: usize) -> u32 {
+    (bytes[i + step_by] == b'M' && bytes[i + 2 * step_by] == b'A' && bytes[i + 3 * step_by] == b'S')
+        as u32
+}
+
+fn positive_directions(bytes: &[u8], i: usize, line_length: usize) -> u32 {
+    if i + 3 * (line_length + 1) < bytes.len() {
+        return positive_check(bytes, i, line_length + 1)
+            + positive_check(bytes, i, line_length)
+            + positive_check(bytes, i, line_length - 1)
+            + positive_check(bytes, i, 1);
+    } else if i + 3 * line_length < bytes.len() {
+        return positive_check(bytes, i, line_length)
+            + positive_check(bytes, i, line_length - 1)
+            + positive_check(bytes, i, 1);
+    } else if i + 3 * (line_length - 1) < bytes.len() {
+        return positive_check(bytes, i, line_length - 1) + positive_check(bytes, i, 1);
+    } else if i + 3 < bytes.len() {
+        return positive_check(bytes, i, 1);
     }
-    let (dx, dy) = direction;
-    if ((i as i32 % line_length) + dx) / line_length != 0 {
-        return false;
+    0
+}
+
+#[inline]
+fn negative_check(bytes: &[u8], i: usize, step_by: usize) -> u32 {
+    (bytes[i - step_by] == b'M' && bytes[i - 2 * step_by] == b'A' && bytes[i - 3 * step_by] == b'S')
+        as u32
+}
+
+fn negative_directions(bytes: &[u8], i: usize, line_length: usize) -> u32 {
+    if i >= 3 * (line_length + 1) {
+        return negative_check(bytes, i, line_length + 1)
+            + negative_check(bytes, i, line_length)
+            + negative_check(bytes, i, line_length - 1)
+            + negative_check(bytes, i, 1);
+    } else if i >= 3 * line_length {
+        return positive_check(bytes, i, line_length)
+            + negative_check(bytes, i, line_length - 1)
+            + negative_check(bytes, i, 1);
+    } else if i >= 3 * (line_length - 1) {
+        return negative_check(bytes, i, line_length - 1) + negative_check(bytes, i, 1);
+    } else if i >= 3 {
+        return negative_check(bytes, i, 1);
     }
-    let next_i = i as i32 + dx + (dy * (line_length));
-    if next_i < 0 || next_i as usize >= bytes.len() {
-        return false;
-    }
-    if bytes[next_i as usize] == state.next().into() {
-        return search(bytes, next_i as usize, line_length, direction, state.next());
-    }
-    return false;
+    0
 }
 
 fn part2(input: &str) -> String {
@@ -82,6 +71,7 @@ fn part2(input: &str) -> String {
     let bytes = input.as_bytes();
     for i in line_lenght + 1..bytes.len() - line_lenght - 1 {
         if bytes[i] == b'A' {
+            // the \n prevents x going past the boundaries of the line
             if (bytes[i + 1 - line_lenght] + bytes[i - 1 + line_lenght] == b'S' + b'M')
                 && (bytes[i + 1 + line_lenght] + bytes[i - 1 - line_lenght] == b'S' + b'M')
             {
--- src/question/day5.rs
+++ src/question/day5.rs
@@ -1,45 +1,63 @@
 // --- Day 5: Print Queue ---
 
+// I'm using some assumptions on the shape of the data:
+// page numbers are 2 digits
+// the page count in a update is always odd
+// there is always *exactly* one valid ordering for incorrectly-ordered updates
+
 use crate::day_solver::{test_day, DaySolver};
 
+#[inline]
+fn parse_page_number(bytes: &[u8], i: usize) -> u8 {
+    (bytes[i] - b'0') * 10 + bytes[i+1] - b'0'
+}
+
 fn part1(input: &str) -> String {
-    let mut lines = input.lines();
-    let ordering_list = construct_ordering_list(&mut lines);
+    let bytes = input.as_bytes();
+    let mut i = 0;
+    let ordering_list = construct_ordering_list(bytes, &mut i);
+    i += 1;
     let mut result = 0;
-    'line: for line in lines {
+    'line: while i < bytes.len() {
         let mut disalowed_pages: u128 = 0;
-        let line_bytes = line.as_bytes();
-        let page_count = (line_bytes.len()+1)/3;
-        for i in 0..page_count {
-            let number = (line_bytes[i*3] - b'0') * 10 + line_bytes[i*3+1] - b'0';
+        let mut page_count: usize = 2;
+        disalowed_pages |= ordering_list[parse_page_number(bytes, i) as usize - 10];
+        i += 3;
+        while bytes[i-1] != b'\n' {
+            let number = parse_page_number(bytes, i);
             if disalowed_pages & (1 << number) != 0 {
                 continue 'line;
             }
             disalowed_pages |= ordering_list[number as usize - 10];
+            i += 3;
+            page_count += 1;
         }
+        println!("{}", bytes[i-1] != b'\n');
+        println!("{disalowed_pages:b}");
         // the number of pages is always odd
         let middle_page_index = (page_count.div_euclid(2))*3;
-        result += ((line_bytes[middle_page_index] - b'0') * 10 + line_bytes[middle_page_index+1] - b'0') as u32;
+        result += parse_page_number(bytes, i - middle_page_index) as u32;
     }
     result.to_string()
 }
 
-fn construct_ordering_list(lines: &mut std::str::Lines) -> [u128; 90] {
+fn construct_ordering_list(bytes: &[u8], i: &mut usize) -> [u128; 90] {
     let mut ordering_list: [u128; 90] = [0; 90];
-    let mut current_line = lines.next().unwrap().as_bytes();
-    while current_line.len() != 0 {
+    let mut next_char = bytes[*i];
+    while next_char != b'\n' {
         // page numbers are Always length 2 in the input
-        let number1 = (current_line[0] - b'0') * 10 + current_line[1] - b'0';
-        let number2 = (current_line[3] - b'0') * 10 + current_line[4] - b'0';
+        let number1 = parse_page_number(bytes, *i);
+        let number2 = parse_page_number(bytes, *i);
         ordering_list[number2 as usize - 10] |= 1 << number1;
-        current_line = lines.next().unwrap().as_bytes();
+        *i += 6;
+        next_char = bytes[*i];
     }
     ordering_list
 }
 
 fn part2(input: &str) -> String {
     let mut lines = input.lines();
-    let ordering_list = construct_ordering_list(&mut lines);
+    let ordering_list = [0; 90]; // construct_ordering_list(&mut lines);
     let mut result = 0;
     for line in lines {
         let mut disalowed_pages = 0;
@@ -48,7 +66,7 @@ fn part2(input: &str) -> String {
         let mut reorder_needed = false;
         let mut pages = 0;
         for i in 0..page_count {
-            let number = (line_bytes[i*3] - b'0') * 10 + line_bytes[i*3+1] - b'0';
+            let number = parse_page_number(line_bytes, i*3);
             if !reorder_needed && disalowed_pages & (1 << number) != 0 {
                 reorder_needed = true;
             }
@@ -66,7 +84,7 @@ fn reorder_pages(mut pages: u128, ordering_list: [u128; 90]) -> u32 {
     let mut pages_left_before_middle = pages.count_ones().div_euclid(2);
     loop {
         let mut pages_iter = pages;
-        while  pages_iter != 0 {
+        loop {
             let page = pages_iter.trailing_zeros();
             if pages & ordering_list[page as usize - 10] == 0 {
                 if pages_left_before_middle <= 0 {
@@ -108,7 +126,8 @@ const SAMPLE_INPUT: &str = "47|53
 75,29,13
 75,97,47,61,53
 61,13,29
-97,13,75,29,47";
+97,13,75,29,47
+";
 
 pub const SOLVER: DaySolver = DaySolver::new(
     part1,
